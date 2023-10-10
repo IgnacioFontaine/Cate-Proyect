c7b85860921a4b0ec8aa55c0ac78a2047bdc7cd0
ignaciofontaine <ignaciofontainebustos@gmail.com>
2023-10-09 21:29:35 -0300

    Cambio oracion

--- client/src/Components/NavBar/navbar.jsx
+++ client/src/Components/NavBar/navbar.jsx
@@ -3,12 +3,12 @@ import Menu from "../Menu/menu";
 
 const NavBar = () => {
   return (
-    <Box display={"flex"}>
-    <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
+    <Box display={"flex"} sx={{width:"75vw"}} >
+    <Box display={"flex"} flexDirection={"column"} alignItems={"center"} >
       <Typography variant="h1">Grupo Juvenil La Catedral</Typography>
       <Typography variant="h4">Gracias por invitarnos a ser Libres, Felices y Santos</Typography>
     </Box>
-      <Box sx={{display:"flex", height:2, textAlign:"center"}}>
+      <Box sx={{display:"flex", height:2, textAlign:"center", alignContent:"end"}}>
         <Menu />
     </Box>
   </Box> 
--- client/src/Components/Oracion/oracion.jsx
+++ client/src/Components/Oracion/oracion.jsx
@@ -4,13 +4,21 @@ const Oracion = () => {
   return (
     <Box>
       <Box>
-        <Typography>Oración de la Cate</Typography>
+        <Typography variant="h2">Oración de la Cate</Typography>
       </Box>
       <Box>
-        <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vero
-        accusantium ut! Ut corrupti accusantium soluta! Delectus, explicabo illo
-        harum ut, consequuntur laudantium, saepe minus nemo obcaecati
-        praesentium excepturi veniam.</Typography>
+        <Typography variant="h4">Señor, que en la cruz de tu hijo Jesús nos mostraste tu grandeza y misericordia, queremos alabarte.
+          Gracias Padre por llamarnos a vivir en la Iglesia, familia de los que creen en Ti.
+          Gracias por amarnos, darnos la vida, nuestras familias y el pan de cada día.
+          Gracias por la Cate que nos acerca a ti.
+          Gracias por invitarnos a ser libres, felices y Santos.
+          Señor, que cada uno de nosotros con la luz del Espíritu Santo, sea testimonio de tu presencia y amor y así hagamos de lo cotidiano la semilla de la santidad.
+          Padre Nuestro que podamos crecer juntos en la Fe, abandonándonos a tu voluntad, en la esperanza siendo luz del mundo y en la caridad, viviendo para dar la vida.
+          Enséñanos a caminar en tu presencia y llenar nuestra vida de tu, como María madre de tu hijo, madre y modelo de todos tus hijos.
+        </Typography>
+        <Typography variant="h5">!Alabado sea Jesucristo,
+          por siempre sea alabado¡
+        </Typography>
       </Box>
     </Box>
   );
